34a1b579ca87c30780fa70073d82a332053bf458
User <User@DESKTOP-C7DGT0E>
2021-11-03 14:40:37 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите N");
+                int N = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите A");
+                double A = double.Parse(Console.ReadLine());
+                Console.WriteLine("Введите B");
+                double B = double.Parse(Console.ReadLine());
+                double H = (B - A) / N;
+                for (double i =A ; i <=B; i+=H)
+                {
+                    Console.WriteLine(1-Math.Sin(i));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+    }
+}
